31476eb080c6bf11fd2040be6915cce17333f6bb
ChaosMAX <supervigito25@gmail.com>
2025-11-11 22:24:45 +0700

    Some change for monitor log

--- monitor.py
+++ monitor.py
@@ -5,15 +5,17 @@ url = "https://chaosdiscord.onrender.com/"  # Đổi thành URL server của b�
 import requests, json
 
 def send_special(name: str):
+    print(name)
     data = {"data": {"user": name}}
     response = requests.post(
         url,
         headers={"Content-Type": "application/json"},
         data=json.dumps(data)
     )
+    print(response.text)
     return response.text
 
 try:
     send_special(str(uuid.uuid4()))
 except:
-    pass
+    print("Error")
